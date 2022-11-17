d9b340359c8fcb925dbfa303c34fc804c42bebf5
andreshapradana <andresha.pradana@gmail.com>
2022-11-17 14:02:58 +0700

    Lab 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:labsatu/form.dart';
-
-
+import 'package:labsatu/page/form.dart';
+import 'package:labsatu/page/to_do_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -100,6 +99,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('To Do'),
+              onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToDoPage()),
                 );
               },
             ),
